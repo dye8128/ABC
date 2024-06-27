a8213ffa78952b486d4d518e7600c1e1bc65f290
dye8128 <daiki.fzn@gmail.com>
2024-06-27 20:43:15 +0900

    .

---
+++ 226D.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using str = string;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+template<class T>using vc = vector<T>;
+template<class T>using vvc = vc<vc<T>>;
+using vint = vc<int>;
+using vll = vc<ll>;
+using vstr = vc<str>;
+using vpint = vc<pint>;
+using vpll = vc<pll>;
+using vbool = vc<bool>;
+using vvint = vvc<int>;
+using vvll = vvc<ll>;
+
+#define overload4(_1, _2, _3, _4, name, ...) name
+#define rep1(n)          for(ll i = 0; i < (n); ++i)
+#define rep2(i, n)       for(ll i = 0; i < (n); ++i)
+#define rep3(i, a, b)    for(ll i = (a); i < (b); ++i)
+#define rep4(i, a, b, c) for(ll i = (a); i < (b); i += (c))
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define each1(a)       for(auto&& i : a)
+#define each2(i,a)     for(auto&& i : a)
+#define each3(x,y,a)   for(auto&& [x, y] : a)
+#define each4(x,y,z,a) for(auto&& [x, y, z] : a)
+#define each(...) overload4(__VA_ARGS__, each4, each3, each2, each1)(__VA_ARGS__)
+#define all(x) (x).begin(), (x).end()
+
+void yesno(bool flag){cout << (flag ? "Yes" : "No") << endl;}
+
+ll gcd(ll a, ll b){
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+
+int main() {
+    ll n; cin >> n;
+    vpll xy(n);
+    rep(n){
+        ll x, y; cin >> x >> y;
+        xy[i] = {x, y};
+    }
+    set<pll> s;
+    rep(n){
+        rep(j,n){
+            if(i==j) continue;
+            ll dx = xy[j].first - xy[i].first;
+            ll dy = xy[j].second - xy[i].second;
+            if(dx == 0){
+                dy /= abs(dy);
+            }else if(dy == 0){
+                dx /= abs(dx);
+            }else{
+                ll g = gcd(abs(dx), abs(dy));
+                dx /= g;
+                dy /= g;
+            }
+            s.insert({dx, dy});
+        }
+    }
+    cout << s.size() << endl;
+}
